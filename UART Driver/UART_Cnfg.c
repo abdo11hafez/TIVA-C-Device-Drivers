43e3ce8c5d1e0de29ab681c50f273fd3e5931687
abdo11hafez <47254796+abdo11hafez@users.noreply.github.com>
2019-02-26 22:41:05 +0200

    Added UART Driver

---
+++ UART Driver/UART_Cnfg.c
@@ -0,0 +1,16 @@
+#include "UART.h"
+#include "UART_Cnfg.h"
+const UART_CfgType UART_ConfigParam [UART_GROUPS_NUMBER] =
+        {
+         {
+          0,
+          0,
+          8,
+          44,
+          3,
+          0,
+          0,
+          0,
+          0,
+         }
+        };
